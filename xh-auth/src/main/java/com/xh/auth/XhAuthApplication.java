5c33bc0e26ff1388052d6776765c23848c88e553
guirong.guo <hgguirong@qq.com>
2025-08-21 00:58:00 +0800

    feat(xh-auth): 添加认证模块
    - 在 pom.xml 中取消注释 xh-auth 模块- 新增 XhAuthApplication 类作为认证模块的入口
    - 添加 XhAuthApplicationTests 类进行基本测试
    - 创建 xh-auth子模块的 pom.xml 文件，定义依赖和插件

---
+++ xh-auth/src/main/java/com/xh/auth/XhAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xh.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhAuthApplication.class, args);
+    }
+
+}
