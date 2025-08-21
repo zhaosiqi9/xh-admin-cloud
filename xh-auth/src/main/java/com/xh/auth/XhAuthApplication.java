d0434b81e854bc4998fe00c0d78a38bf125b76df
guirong.guo <15000222148@139.com>
2025-08-22 00:16:42 +0800

    feat(xh-admin-cloud): 添加 Nacos 配置和发现功能- 在 xh-auth 模块中添加 Nacos 配置- 新增 xh-gateway模块，实现 Nacos 发现和配置功能 - 添加 xh-common 模块作为公共依赖- 更新根 pom.xml，包含新模块

--- xh-auth/src/main/java/com/xh/auth/XhAuthApplication.java
+++ xh-auth/src/main/java/com/xh/auth/XhAuthApplication.java
@@ -8,6 +8,7 @@ public class XhAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XhAuthApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  xh-auth启动成功   ლ(´ڡ`ლ)ﾞ  ");
     }
 
 }
---
+++ xh-gateway/src/test/java/com/xh/gateway/XhGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xh.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
