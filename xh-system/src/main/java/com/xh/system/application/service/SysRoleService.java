14bc9853a1615f7a69d5d28f1f379f22959bed99
guirong.guo <1107719463@qq.com>
2025-09-08 23:44:13 +0800

    feat(system): 新增系统角色相关功能
    - 添加系统角色数据权限和角色权限DTO类
    - 实现角色查询、保存、删除等功能
    - 添加角色菜单权限和数据权限相关方法
    - 优化角色权限保存逻辑，支持递归删除子角色多余权限

--- xh-system/src/main/java/com/xh/system/application/service/SysRoleService.java
+++ xh-system/src/main/java/com/xh/system/application/service/SysRoleService.java
@@ -8,20 +8,29 @@ import com.xh.common.base.web.PageResult;
 import com.xh.system.api.request.RoleQueryRequest;
 import com.xh.system.api.response.RoleQueryResponse;
 import com.xh.system.application.command.sysrole.RoleQueryCommand;
-import com.xh.system.application.mapstract.SysOrgEntity2ResponseMapper;
+import com.xh.system.application.command.sysrole.SysRoleDataPermission;
+import com.xh.system.application.command.sysrole.SysRolePermissionDTO;
 import com.xh.system.application.mapstract.SysRoleEntity2ResponseMapper;
-import com.xh.system.domain.entity.SysOrg;
+import com.xh.system.domain.entity.SysMenu;
 import com.xh.system.domain.entity.SysRole;
+import com.xh.system.domain.entity.SysRoleMenu;
+import com.xh.system.domain.service.SysMenuDomainService;
 import com.xh.system.domain.service.SysUserDomainService;
 import com.xh.system.infrastructure.mysql.po.SysRolePO;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class SysRoleService {
 
     @Resource
     private SysUserDomainService sysUserDomainService;
+
+    @Resource
+    private SysMenuDomainService sysMenuDomainService;
     
     public PageResult<RoleQueryResponse> query(PageQuery<RoleQueryRequest> request) {
         MPJLambdaWrapper<SysRolePO> lambdaWrapper = RoleQueryCommand.initQueryWrapper(request);
@@ -29,6 +38,63 @@ public class SysRoleService {
         Page<SysRole> rolePage = SysUserDomainService.getRepository(SysUserConstant.SysUserRootType.ROLE).sysRolePageQuery(request.getCurrentPage(),
                 request.getPageSize(), lambdaWrapper);
         return SysRoleEntity2ResponseMapper.INSTANCE.pageEntity2PageResult(rolePage);
+        
+    }
+
+    public void del(List<Long> ids) {
+        
+    }
+
+    public SysRole getById(Long id) {
+        return null;
+    }
+
+    public SysRole save(SysRole sysRole) {
+        
+        if(sysRole.getId())
+        
+        if (sysRole.getId() == null) {
+            baseJdbcDao.insert(sysRole);
+        } else {
+            //删除之前的权限
+            String sql = "delete from sys_role_menu where sys_role_id = ?";
+            primaryJdbcTemplate.update(sql, sysRole.getId());
+            baseJdbcDao.update(sysRole);
+        }
+        List<SysRoleMenu> roleMenus = sysRole.getRoleMenus();
+        for (SysRoleMenu roleMenu : roleMenus) {
+            roleMenu.setSysRoleId(sysRole.getId());
+            baseJdbcDao.insert(roleMenu);
+        }
+        //需要删除子级角色多出的权限（mysql8.0递归查询并删除）
+        String sql2 = """
+                WITH recursive tb as (
+                	SELECT * from sys_role where parent_id = ?
+                	UNION ALL
+                	SELECT b.* from tb inner join sys_role b on b.parent_id = tb.id
+                )
+                DELETE FROM sys_role_menu
+                WHERE sys_role_id IN ( SELECT id FROM tb )
+                    AND sys_menu_id NOT IN (
+                     select * from (select sys_menu_id from sys_role_menu where sys_role_id = ?) temp
+                    )
+                """;
+        primaryJdbcTemplate.update(sql2, sysRole.getId(), sysRole.getId());
+
+        //刷新角色权限
+        commonService.getRolePermissions(sysRole.getId(), true);
+        return sysRole;
+    }
+
+    public List<SysMenu> queryRoleMenu(Map<String, Object> param) {
+        return null;
+    }
+
+    public List<SysRoleDataPermission> queryRoleDataPermission(SysRolePermissionDTO sysRolePermissionDTO) {
+        return null;
+    }
+
+    public void saveRoleDataPermission(SysRolePermissionDTO sysRolePermissionDTO) {
         
     }
 }
--- xh-system/src/main/java/com/xh/system/interfaces/SysRoleController.java
+++ xh-system/src/main/java/com/xh/system/interfaces/SysRoleController.java
@@ -1,18 +1,24 @@
 package com.xh.system.interfaces;
 
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaMode;
 import com.xh.common.base.web.PageQuery;
 import com.xh.common.base.web.PageResult;
 import com.xh.common.base.web.RestResponse;
 import com.xh.system.api.request.RoleQueryRequest;
 import com.xh.system.api.response.RoleQueryResponse;
+import com.xh.system.application.command.sysrole.SysRoleDataPermission;
+import com.xh.system.application.command.sysrole.SysRolePermissionDTO;
 import com.xh.system.application.service.SysRoleService;
+import com.xh.system.domain.entity.SysMenu;
+import com.xh.system.domain.entity.SysRole;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 
@@ -25,5 +31,45 @@ public class SysRoleController {
     @PostMapping("/query")
     public RestResponse<PageResult<RoleQueryResponse>> query(@RequestBody PageQuery<RoleQueryRequest> request){
         return RestResponse.success(Optional.ofNullable(request).map(t -> sysRoleService.query(t)).orElse(null));
+    }
+    
+    @Operation(description = "角色保存")
+    @PostMapping("/save")
+    public RestResponse<SysRole> save(@RequestBody SysRole sysRole) {
+        return RestResponse.success(sysRoleService.save(sysRole));
+    }
+
+    @Operation(description = "获取角色详情")
+    @GetMapping("/get/{id}")
+    public RestResponse<SysRole> getById(@PathVariable Long id) {
+        return RestResponse.success(sysRoleService.getById(id));
+    }
+
+    @Operation(description = "角色批量删除")
+    @DeleteMapping("/del")
+    public RestResponse<?> del(@RequestParam List<Long> ids) {
+        sysRoleService.del(ids);
+        return RestResponse.success();
+    }
+
+    @Operation(description = "查询角色可配置的所有菜单权限")
+    @GetMapping("/queryRoleMenu")
+    public RestResponse<List<SysMenu>> queryRoleMenu(@RequestParam Map<String, Object> param) {
+        List<SysMenu> roleMenus = sysRoleService.queryRoleMenu(param);
+        return RestResponse.success(roleMenus);
+    }
+
+    @Operation(description = "查询角色的数据权限")
+    @GetMapping("/queryRoleDataPermission")
+    public RestResponse<List<SysRoleDataPermission>> queryRoleDataPermission(SysRolePermissionDTO sysRolePermissionDTO) {
+        List<SysRoleDataPermission> roleDataPermission = sysRoleService.queryRoleDataPermission(sysRolePermissionDTO);
+        return RestResponse.success(roleDataPermission);
+    }
+
+    @Operation(description = "保存角色数据权限")
+    @PostMapping("/saveRoleDataPermission")
+    public RestResponse<?> saveRoleDataPermission(@RequestBody SysRolePermissionDTO sysRolePermissionDTO) {
+        sysRoleService.saveRoleDataPermission(sysRolePermissionDTO);
+        return RestResponse.success();
     }
 }
