24c7d4e8de4136942b93f123898d7094fe3b78e3
guirong.guo <1107719463@qq.com>
2025-09-11 00:23:57 +0800

    refactor(xh-api):调整模块结构和安全配置- 移除 xh-auth-api 模块 - 更新 xh-common-jwt 自动配置 - 新增 SecurityConfiguration 类实现统一安全配置 - 添加 xh-common-security 自动配置

---
+++ xh-common/xh-common-security/src/main/java/com/xh/common/security/config/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.xh.common.security.config;
+
+import cn.dev33.satoken.SaManager;
+import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.same.SaSameUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@AutoConfiguration
+public class SecurityConfiguration implements WebMvcConfigurer {
+    
+    /**
+     * 注册sa-token的拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册路由拦截器，自定义验证规则
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+    }
+
+    /**
+     * 校验是否从网关转发
+     */
+    @Bean
+    public SaServletFilter getSaServletFilter() {
+        return new SaServletFilter()
+            .addInclude("/**")
+            .addExclude("/actuator", "/actuator/**")
+            .setAuth(obj -> {
+                if (SaManager.getConfig().getCheckSameToken()) {
+                    SaSameUtil.checkCurrentRequestToken();
+                }
+            })
+            .setError(e -> SaResult.error("认证失败，无法访问系统资源").setCode(HttpStatus.UNAUTHORIZED.value()));
+    }
+
+    /**
+     * 对 actuator 健康检查接口 做账号密码鉴权
+     */
+    @Bean
+    public SaServletFilter actuatorFilter() {
+//        String username = SpringUtils.getProperty("spring.cloud.nacos.discovery.metadata.username");
+//        String password = SpringUtils.getProperty("spring.cloud.nacos.discovery.metadata.userpassword");
+        return new SaServletFilter()
+            .addInclude("/actuator", "/actuator/**")
+//            .setAuth(obj -> {
+//                SaHttpBasicUtil.check(username + ":" + password);
+//            })
+            .setError(e -> SaResult.error(e.getMessage()).setCode(HttpStatus.UNAUTHORIZED.value()));
+    }
+}
