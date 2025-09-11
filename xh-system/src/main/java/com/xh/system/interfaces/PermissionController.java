fb45d032e3c3059357b7a70aac08f1ef5ef93cfc
guirong.guo <1107719463@qq.com>
2025-09-11 23:47:10 +0800

    feat(system): 添加数据权限相关接口
    - 新增数据实体列表查询

--- xh-system/src/main/java/com/xh/system/interfaces/PermissionController.java
+++ xh-system/src/main/java/com/xh/system/interfaces/PermissionController.java
@@ -1,13 +1,18 @@
 package com.xh.system.interfaces;
 
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaMode;
+import com.xh.common.base.web.PageQuery;
+import com.xh.common.base.web.RestResponse;
 import com.xh.common.core.api.contract.RemotePermissionContract;
 import com.xh.system.application.service.PermissionService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author : gr
@@ -37,4 +42,40 @@ public class PermissionController implements RemotePermissionContract {
         }
         return permissionService.getRoleList(loginId, loginType);
     }
+    
+/*     @Operation(description = "数据实体列表查询")
+    @PostMapping("/entity/query")
+    public RestResponse<List<SysDataEntity>> queryEntity() {
+        List<SysDataEntity> data = sysDataPermissionService.queryEntity();
+        return RestResponse.success(data);
+    }
+
+    @Operation(description = "数据权限列表查询")
+    @PostMapping("/query")
+    public RestResponse<PageResult<SysDataPermission>> query(@RequestBody PageQuery<Map<String, Object>> pageQuery) {
+        PageResult<SysDataPermission> data = sysDataPermissionService.query(pageQuery);
+        return RestResponse.success(data);
+    }
+
+    @SaCheckPermission(value = {"system:dataPermission:add", "system:dataPermission:edit"}, mode = SaMode.OR)
+    @Operation(description = "数据权限保存")
+    @PostMapping("/save")
+    public RestResponse<SysDataPermission> save(@RequestBody SysDataPermission sysDataPermission) {
+        return RestResponse.success(sysDataPermissionService.save(sysDataPermission));
+    }
+
+    @SaCheckPermission(value = {"system:dataPermission:edit", "system:dataPermission:detail"}, mode = SaMode.OR)
+    @Operation(description = "获取数据权限详情")
+    @GetMapping("/get/{id}")
+    public RestResponse<SysDataPermission> getById(@PathVariable Integer id) {
+        return RestResponse.success(sysDataPermissionService.getById(id));
+    }
+
+    @SaCheckPermission("system:dataPermission:del")
+    @Operation(description = "数据权限批量删除")
+    @DeleteMapping("/del")
+    public RestResponse<?> del(@RequestParam List<Integer> ids) {
+        sysDataPermissionService.del(ids);
+        return RestResponse.success();
+    }*/
 }
